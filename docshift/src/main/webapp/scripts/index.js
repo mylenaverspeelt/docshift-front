aebfe7a63c653014fde77384bf13b745953be0e5
mylenaverspeelt <mylena49@hotmail.com>
2023-10-24 09:54:04 -0300

    improve home page css

--- docshift/src/main/webapp/scripts/index.js
+++
@@ -1,9 +0,0 @@
-function do_something(){
-	const elem = document.getElementById("my-elem");
-	elem.innerHTML = "Teste 123";
-}
-
-document.addEventListener("DOMContentLoaded", function (){
-	do_something();	
-})
-
